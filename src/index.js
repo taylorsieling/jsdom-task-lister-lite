4e446d07ce4b7d48b73d88039d22585ebf25642d
taylorsieling <taylorasieling@gmail.com>
2021-03-02 14:21:37 -0600

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  form.addEventListener("submit", manageListItems);
 });
+
+const form = document.getElementById("create-task-form");
+const taskDesc = document.getElementById("new-task-description");
+
+function manageListItems(event) {
+  event.preventDefault();
+  console.log("Submit was clicked");
+
+  // creating a new list item
+
+  const tasks = document.getElementById('tasks');
+  const newTask = document.createElement('li');
+  console.log(taskDesc.value);
+  newTask.textContent = taskDesc.value;
+  tasks.appendChild(newTask);
+  form.reset();
+
+}
+
+    
